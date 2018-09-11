de40616e19247a3bf4f6b38726502e55bb113e5b
samitbarai <samit.barai@juspay.in>
2018-09-11 12:59:04 +0530

    initial commit

---
+++ src/components/counters.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Counter from './counter'
+
+class Counters extends Component {
+
+    render() {
+        const { onReset, onIncrement, onDelete, counters } = this.props;
+        return (
+            <div>
+                <button 
+                    onClick={onReset}
+                    className="btn btn-primary btn-sm m-2"
+                >
+                    Reset
+                </button>
+                {counters.map(counter =>
+                    <Counter 
+                        key={counter.id}
+                        onDelete={onDelete}
+                        onIncrement={onIncrement}
+                        value={counter.value}
+                        counter={counter}
+                    />
+                )}
+            </div>
+         );
+    }
+}
+ 
+export default Counters;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+//Stateless functional component
+
+const NavBar = ({ totalCounters }) => {
+    
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+                Navbar
+                <span className="badge badge-pill badge-secondary m-2">
+                    {totalCounters}
+                </span>
+            </a>
+        </nav>
+    );
+};
+
+ 
+export default NavBar;

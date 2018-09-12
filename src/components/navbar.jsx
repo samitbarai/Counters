4d11f55c90672e1de2f8101b27c96442576c53e2
samitbarai <samit.barai@juspay.in>
2018-09-12 11:37:38 +0530

    Renamed Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,7 +7,7 @@ const NavBar = ({ totalCounters }) => {
     return (
         <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">
-                Navbar
+                No. of Items
                 <span className="badge badge-pill badge-secondary m-2">
                     {totalCounters}
                 </span>
